9f11df97102c64a8d14ae75f27cbe8cbaa854bb4
Ryota Goto <pandoraxssscan.929@gmail.com>
2018-01-05 17:31:33 +0900

    頂点シェーダ－とピクセルシェーダーを環境反射に対応

--- 3DCGLib/PixelShader.hlsl
+++ 3DCGLib/PixelShader.hlsl
@@ -1,10 +1,24 @@
-struct VS_OUTPUT
+cbuffer ConstantBuffer : register(b0)
 {
-    float4 Pos : SV_POSITION;
-    float4 Color : COLOR0;
+    matrix World;           // ワールド行列
+    matrix View;            // ビュー行列
+    matrix Projection;      // 射影行列
+    float4 lightAmbient;    // 環境光
+    float4 materialAmbient; // 物体の色
 };
 
-float4 PS(VS_OUTPUT input) : SV_Target
+struct PS_INPUT
 {
-    return input.Color;
+    float4 Pos : SV_POSITION; // 現在のピクセル位置
+    float4 PosW : POSITION0;  // オブジェクトのワールド座標
+    float4 NorW : TEXCOORD0;  // 法線
+};
+
+float4 PS(PS_INPUT input) : SV_TARGET
+{
+    float3 i;
+
+    i = materialAmbient.xyz * lightAmbient.xyz;
+
+    return float4(i, 1.0);
 }
